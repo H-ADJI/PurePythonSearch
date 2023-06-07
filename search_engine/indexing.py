bcb83e7f11fc8debc66c8646b18e86f734b3fcde
h-adji <h-adji_tech@proton.me>
2023-06-07 15:38:40 +0100

    addapted logic so that data is retrieved from db

--- search_engine/indexing.py
+++ search_engine/indexing.py
@@ -1,112 +1,128 @@
-'''
+"""
 File: indexing.py
 File Created: Thursday, 2nd February 2023 4:42:49 pm
 Author: KHALIL HADJI 
 -----
 Copyright:  KHALIL HADJI 2023
-'''
+"""
 import math
-from .data_cleaning import full_parse, remove_separator, remove_punctuation
 from dataclasses import dataclass
-from typing import Optional, Literal
-from collections import Counter
-from .utils import timer
+from typing import Literal
+
+from search_engine.database import Jobs, get_job_by_id, job_generator
+
+from .data_processing import tokenize
 
 
 @dataclass
-class Job():
-    id: Optional[str]
-    url: Optional[str]
-    title: Optional[str]
-    description: Optional[str]
-
-    @property
-    def clean_tokens(self):
-        return full_parse(self.title + " " + self.description)
-
-    def generate_counter_bag(self):
-        self.bag_of_words = Counter(self.clean_tokens)
-
-    def term_frequency(self, term):
-        return self.bag_of_words.get(term, 0)
+class SearchResult:
+    relevancy_score: float
+    document: Jobs
 
 
-class Index:
+class SearchEngine:
+    """
+    Search Engine Dyal Lay7sen L3wan
+    """
+
     def __init__(self):
-        """ 
-        index : is an inverted index that will map a token to the set of all documents id where the token appears
-        documents : mapping ids to job documents
         """
-        self.index: dict[str, set] = {}
-        self.documents: dict[str, Job] = {}
+        inverted_index : an inverted index that map a token to
+        the set of all documents ids where the token appears
+        documents_index : ids of the indexed documents
+        """
+        self.inverted_index: dict[str, set] = {}
+        self.documents_index: set[int] = set()
 
-    def index_document(self, job: Job):
-        if job.id not in self.documents:
-            self.documents[job.id] = job
-            job.generate_counter_bag()
+    def load_data(self):
+        for job in job_generator():
+            self._index_document(job=job)
+
+    def _index_document(self, job: Jobs):
+        if job.id not in self.documents_index:
+            self.documents_index.add(job.id)
 
         for token in job.clean_tokens:
-            if token not in self.index:
-                self.index[token] = set()
-            self.index[token].add(job.id)
+            if token not in self.inverted_index:
+                self.inverted_index[token] = set()
+            self.inverted_index[token].add(job.id)
 
-    def _document_frequency(self, token: str):
-        return len(self.index.get(token, set()))
+    def _document_frequency(self, token: str) -> int:
+        """A measure of abundance :
+        Count how much documents contains a specific token
 
-    def _inverse_document_frequency(self, token: str):
-        return math.log2(len(self.documents)/self._document_frequency(token=token))
 
-    def _results(self, parsed_query):
-        return [self.index.get(token, set()) for token in parsed_query]
+        Args:
+            token (str): the token we're looking for
 
-    def _rank(self, parsed_query: list[str], document_ids: list[str]):
+        Returns:
+            int: the how many documents contain the token
+        """
+        return len(self.inverted_index.get(token, set()))
+
+    def _inverse_document_frequency(self, token: str) -> float:
+        """Computes the inverse document frequency of a token:
+        how many documents we have on our index divided by
+        the number of documents that contains the token
+        its a kind of popularity measure to decide if the token contributes
+        to the special meaning of the document
+
+        Args:
+            token (str): a text token
+
+        Returns:
+            float: the ratio of the number of documents and the abundance of the token
+        """
+        return math.log2(
+            len(self.documents_index) / self._document_frequency(token=token)
+        )
+
+    def _results(self, parsed_query: list):
+        """return a list containing the ids of documents that contains a specific token
+
+        Args:
+            parsed_query (list): tokens of the input search query
+
+        Returns:
+            list: Documents ids
+        """
+
+        return [self.inverted_index.get(token, set()) for token in parsed_query]
+
+    def _rank(
+        self, parsed_query: list[str], document_ids: list[str]
+    ) -> list[SearchResult]:
         results = []
         if not document_ids:
             return results
         for id in document_ids:
             doc_relevancy_score = 0
+            document: Jobs = get_job_by_id(id)
+
             for query_term in parsed_query:
-                tf = self.documents.get(id).term_frequency(term=query_term)
-                idf = self._inverse_document_frequency(token=query_term)
-                doc_relevancy_score += tf*idf
-            results.append((id, doc_relevancy_score))
-        return sorted(results, key=lambda x: x[1], reverse=True)
+                tf = document.term_frequency(term=query_term)
+                idf = 0
+                # if a query token
+                if tf:
+                    idf = self._inverse_document_frequency(token=query_term)
+                doc_relevancy_score += tf * idf
+            results.append(
+                SearchResult(relevancy_score=doc_relevancy_score, document=document)
+            )
 
-    @timer
-    def search(self, query, rank=True, search_type: Literal["AND", "OR"] = "OR"):
-        parsed_query = full_parse(text=query)
+        return sorted(results, key=lambda x: x.relevancy_score, reverse=True)
+
+    def search(self, query: str, rank=True, search_type: Literal["AND", "OR"] = "OR"):
+        parsed_query = tokenize(text=query)
         results = self._results(parsed_query=parsed_query)
-        if results:
-            if search_type == "AND":
-                hits = set.intersection(*results)
-            else:
-                hits = set.union(*results)
-            if rank:
-                results = self._rank(parsed_query=parsed_query,
-                                     document_ids=hits)
-                for id, score in results:
-                    yield f" ==> job title : {self.documents.get(id).title} \n ==> job url : {self.documents.get(id).url} \n  SCORE : {score}"
-            else:
-                for id in hits:
-                    yield f" ==> job title : {self.documents.get(id).title} \n ==> job url : {self.documents.get(id).url} \n "
-
-
-@timer
-def load_data_to_index(index: Index, file_path: str = "job_details.csv"):
-    """
-    sample data adapte the data class according to the data you're using
-    """
-    with open("job_data.csv", 'r') as jobs_file:
-        jobs_file.readline()
-        while line := jobs_file.readline():
-            data_list = line.split("~")
-
-            data = {
-                "id": data_list[0],
-                "title": data_list[8],
-                "description": data_list[1],
-                "url": data_list[9]
-            }
-            job = Job(**data)
-            index.index_document(job)
-    return index
+        if search_type == "AND":
+            hits = set.intersection(*results)
+        else:
+            hits = set.union(*results)
+        if rank:
+            results = self._rank(parsed_query=parsed_query, document_ids=hits)
+            for search_result in results:
+                yield f" ==> job title : {search_result.document.title} \n ==> SCORE : {search_result.relevancy_score}"  # noqa E501
+        else:
+            for id in hits:
+                yield f" ==> job title : {get_job_by_id(id).title} "  # noqa E501
